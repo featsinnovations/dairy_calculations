fbd5971b2a1c106b9669590aa3c8c865e37882af
Henil00 <henilpatel11111975@gmail.com>
2025-04-10 18:11:59 +0530

    Added Immages url

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-    domains: ['localhost'],
+    domains: ['localhost','https://dairy-app-backend-3z13.onrender.com/'],
   },
  
 };
